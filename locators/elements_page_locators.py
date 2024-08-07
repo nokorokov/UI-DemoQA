f215c25015c0ba46952a492dcb9171288a33caf2
Nik <nikitaokorokov@yahoo.com>
2024-08-07 16:43:05 +0300

    update elements tests added test checkboxes

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -16,8 +16,10 @@ class TextBoxPageLocators:
     CREATED_CURRENT_ADDRESS = (By.CSS_SELECTOR, "#output #currentAddress")
     CREATED_PERMANENT_ADDRESS = (By.CSS_SELECTOR, "#output #permanentAddress")
 
-    # # created from
-    # CREATED_FULL_NAME = (By.CSS_SELECTOR, "p[id='name']")
-    # CREATED_EMAIL = (By.CSS_SELECTOR, "p[id='email']")
-    # CREATED_CURRENT_ADDRESS = (By.CSS_SELECTOR, "p[id='currentAddress']")
-    # CREATED_PERMANENT_ADDRESS = (By.CSS_SELECTOR, "p[id='permanentAddress']")
+
+class CheckBoxPageLocators:
+    EXPAND_ALL_BUTTON = (By.CSS_SELECTOR, 'button[title="Expand all"]')
+    ITEM_LIST = (By.CSS_SELECTOR, 'span[class="rct-title"]')
+    CHECKED_ITEMS = (By.CSS_SELECTOR, 'svg[class = "rct-icon rct-icon-check"]')
+    TITLE_ITEM = './/ancestor::span[@class = "rct-text"]'
+    OUTPUT_RESULT = (By.CSS_SELECTOR, 'span[class = "text-success"]')
