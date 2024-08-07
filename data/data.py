9640928ed29de4c6ae1ab6c40902811110ac29b9
Nik <nikitaokorokov@yahoo.com>
2024-08-07 21:06:24 +0300

    add property's to class person: firstname, lastname, age, salary, department

--- data/data.py
+++ data/data.py
@@ -3,7 +3,13 @@ from dataclasses import dataclass
 
 @dataclass
 class Person:
+    firstname: str = None
+    lastname: str = None
     full_name: str = None
     email: str = None
+    age: int = None
+    salary: float = None
+    department: str = None
     current_address: str = None
     permanent_address: str = None
+
--- generator/generator.py
+++ generator/generator.py
@@ -1,3 +1,5 @@
+import random
+
 from data.data import Person
 from faker import Faker
 
@@ -7,8 +9,14 @@ Faker.seed()
 
 def generated_person():
     yield Person(
+        firstname=faker_ru.first_name(),
+        lastname=faker_ru.last_name(),
         full_name=faker_ru.first_name() + " " + faker_ru.last_name() + " " + faker_ru.middle_name(),
+        age=random.randint(10, 80),
+        department=faker_ru.job(),
+        salary=random.randint(10000, 20000),
         email=faker_ru.email(),
         current_address=faker_ru.address(),
         permanent_address=faker_ru.address()
+
     )
