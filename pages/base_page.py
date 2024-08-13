4b5fd5d14cd28adcc9a174f47a36d6d6f799b13e
nokorokov <n.okorokov@itpelag.com>
2024-08-13 11:59:51 +0300

    Added func to base_page: - remove_footer

--- pages/base_page.py
+++ pages/base_page.py
@@ -54,3 +54,7 @@ class BasePage:
         action.context_click(element)
         action.perform()
 
+    def remove_footer(self):
+        self.driver.execute_script("document.getElementsByTagName('footer')[0].remove();")
+        self.driver.execute_script("document.getElementById('close-fixedban').remove();")
+
