0cc931d1bef3fe97d8f5eadc8a465218b165d0e2
nokorokov <n.okorokov@itpelag.com>
2024-08-21 14:57:43 +0300

    added new method/locator/test: - sortable page
    add new methods in base_page:
    - action_drag_and_drop_to_element

---
+++ tests/interactions_test.py
@@ -0,0 +1,18 @@
+from conftest import driver
+from pages.interactions_page import (SortablePage)
+
+
+class TestInteractions:
+    class TestSortablePage:
+
+        def test_sortable(self, driver):
+            sortable_page = SortablePage(driver, 'https://demoqa.com/sortable')
+            sortable_page.open()
+            order_before, order_after = sortable_page.change_list_order()
+            assert order_before != order_after
+
+        def test_grid_sortable(self, driver):
+            sortable_page = SortablePage(driver, 'https://demoqa.com/sortable')
+            sortable_page.open()
+            order_before, order_after = sortable_page.change_grid_order()
+            assert order_before != order_after
