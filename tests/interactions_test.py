fa34d05c5845f796a791f8482a7487f8e1b2b683
nokorokov <n.okorokov@itpelag.com>
2024-08-21 15:56:22 +0300

    added new method/locator/test: - selectable page

--- tests/interactions_test.py
+++ tests/interactions_test.py
@@ -1,5 +1,5 @@
 from conftest import driver
-from pages.interactions_page import (SortablePage)
+from pages.interactions_page import (SortablePage, SelectablePage)
 
 
 class TestInteractions:
@@ -16,3 +16,13 @@ class TestInteractions:
             sortable_page.open()
             order_before, order_after = sortable_page.change_grid_order()
             assert order_before != order_after
+
+    class TestSelectablePage:
+
+        def test_selectable(self, driver):
+            selectable_page = SelectablePage(driver, 'https://demoqa.com/selectable')
+            selectable_page.open()
+            item_list = selectable_page.select_list_items()
+            item_grid = selectable_page.select_grid_items()
+            assert len(item_list) > 0, 'no elements were selected'
+            assert len(item_grid) > 0, 'no elements were selected'
